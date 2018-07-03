ab57a3f1b5f95939761d6a44756c22f22ef22503
Austin Moninger <austin.moninger@rice.edu>
2018-07-02 21:31:38 -0500

    Update and rename class-26.ino to button.ino

--- button.ino
+++ button.ino
@@ -4,22 +4,11 @@
  Turns on and off a light emitting diode(LED) connected to digital  
  pin 13, when pressing a pushbutton attached to pin 7. 
  
- 
  The circuit:
  * LED attached from pin 13 to ground 
  * pushbutton attached to pin 2 from +5V
  * 10K resistor attached to pin 2 from ground
  
- * Note: on most Arduinos there is already an LED on the board
- attached to pin 13.
- 
- 
- created 2005
- by DojoDave 
- modified 17 Jun 2009
- by Tom Igoe
- 
-  http://www.arduino.cc/en/Tutorial/Button
  */
 
 int ledPin = 13; // choose the pin for the LED 
